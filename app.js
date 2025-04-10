d9ba8328bd77e263dc301cf43ad8158c02169699
JulianChildon <blaytmanbit@gmail.com>
2025-04-10 23:39:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function handleTranslate() {
         isTranslating = true;
         showStatus('翻译中...', 'loading');
         
-        const response = await fetch('https://api.render.com/deploy/srv-cvrt4p24d50c73d5i7kg?key=NXyuvG6sTRc', {
+        const response = await fetch('https://translatorh5-test.onrender.com/translate', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
