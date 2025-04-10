22d4cee66b5a0338d3e93e101563b43ec38bb6b3
JulianChildon <blaytmanbit@gmail.com>
2025-04-10 22:55:51 +0800

    接入修正

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ function getAuthStr(date, digest) {
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' ,() => {
     console.log(`Server running on port ${PORT}`);
 });
 
